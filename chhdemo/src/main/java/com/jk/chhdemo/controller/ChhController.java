26fa3370a88de7a7fa84f096656ede13110e5737
yaofatong <347590061@QQ.com>
2019-06-12 17:36:22 +0800

    姚法通6.12 17.35提交

---
+++ chhdemo/src/main/java/com/jk/chhdemo/controller/ChhController.java
@@ -0,0 +1,14 @@
+package com.jk.chhdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ChhController {
+    @RequestMapping("cc")
+    @ResponseBody
+    public String test(){
+        return "dddd";
+    }
+}
---
+++ yftdemo/src/main/java/com/jk/yftdemo/YftdemoApplication.java
@@ -0,0 +1,13 @@
+package com.jk.yftdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YftdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YftdemoApplication.class, args);
+    }
+
+}
